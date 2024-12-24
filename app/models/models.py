91cfe8f4a42547c36020e3728fcdc8f72aae8872
Or Muller <ormullerh@gmail.com>
2024-12-24 03:34:23 -0500

    add all models needed so far

--- app/models/models.py
+++ app/models/models.py
@@ -1,15 +1,14 @@
 import datetime
-from pydantic import BaseModel
+
+from pydantic import BaseModel,Field
 import datetime as dt
 class DateRange(BaseModel):
-    start_date: str
-    end_date: str
-class WorkItemFetchRequest(BaseModel):
-    start_date: str
-    end_date: str
+    start_date: datetime.date
+    end_date: datetime.date
+
 class Commit(BaseModel):
     id: str
-    datetime: datetime.datetime
+    datetime: datetime.date = Field(..., alias="datetime", example="2021-24-01")
     message: str
     status: str
 class WorkItem(BaseModel):
@@ -19,8 +18,15 @@ class WorkItem(BaseModel):
     status: str
     assigned_to: str
 class Document(BaseModel):
-    title: str
-    content: str
+    title: str = Field(..., alias="title",description="title of document")
+    content: str = Field(..., alias="content",description="content of document")
+
+class Sprint(BaseModel):
+    id: str | None = Field(None, alias="id")
+    name: str = Field(..., alias="name")
+    start_date: datetime.date = Field(..., alias="start_date", example="2021-24-01")
+    end_date: datetime.date =Field(..., alias="end_date", example="2021-24-01")
+    status: str = Field(..., alias="status",description="status of sprint")
 
 
 
